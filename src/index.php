e6c5b49e29d55db28656947ae23991283b96f9b9
Jinoy <jinoy.v2000@gmail.com>
2024-09-08 20:33:22 +0530

    insertion

--- src/index.php
+++ src/index.php
@@ -1,1 +1,26 @@
-<?php echo "hello world"; ?>
+<?php 
+require 'vendor/autoload.php';
+ParseClient::initialize( "zBYDXMfPx8ArjqWttOX9G2Vx0pdsvK4U3t7dN4Va", "VIdmRLrOoMq6jDS3aQiq6CZ7m6Xp96TmMmjKoWF9", "A2lDwdvyo5yXwmHOkFO85C2HUbPaOnW6MRwreIaD" );
+ParseClient::setServerURL('https://parseapi.back4app.com', '/');
+use Parse\ParseException;
+use Parse\ParseObject;
+
+$myCustomObject = new ParseObject("orders");
+
+$myCustomObject->set("order_id", "1");
+$myCustomObject->set("item_name", "Pen");
+$myCustomObject->set("name", "Jinoy");
+
+
+try {
+  $myCustomObject->save();
+  echo 'New object created with objectId: ' . $myCustomObject->getObjectId();
+} catch (ParseException $ex) {
+  // Execute any logic that should take place if the save fails.
+  // error is a ParseException object with an error code and message.
+  echo 'Failed to create new object, with error message: ' . $ex->getMessage();
+}
+
+
+
+?>
---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5d13ab88e58dfe4e8d048fa8f8a55745603f618a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5d13ab88e58dfe4e8d048fa8f8a55745603f618a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'parse/php-sdk' => array(
+            'pretty_version' => '1.4.0',
+            'version' => '1.4.0.0',
+            'reference' => '19deeb473a33d9cc15271ef78f7520d95fa792ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../parse/php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
